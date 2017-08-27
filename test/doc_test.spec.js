e1140be865225ef2414603e79968c49dbdaf018b
Ari Mourao <amlima@gmail.com>
2017-08-27 16:18:19 -0300

    Initial commit

---
+++ test/doc_test.spec.js
@@ -0,0 +1,30 @@
+describe('PasswordController', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.grade', function(){
+    var $scope, controller;
+    
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('PasswordController', {$scope: $scope});
+    });
+
+    it('sets the strength to "strong" if the password length is >8 chars', function(){
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+    });
+
+    it('sets the strength to "weak if the password length <3', function(){
+      $scope.password = 'a';
+      $scope.grade();
+      expect($scope.strength).toEqual('weak');
+    });
+  });
+});
