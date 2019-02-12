4658c187e7898f16115229e111ce6b6e85ad5aa4
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-02-12 15:57:49 -0200

    updates webpack's config (ref #7)

--- webpack.config.js
+++ webpack.config.js
@@ -2,68 +2,63 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
 });
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'assets')
-  },
-  plugins: {
-    uglify: {
-      mangle: false,
-      compress: {
-        global_defs: {
-          DEBUG: false
-        }
-      }
-    }
-  },
-  module: {
-    loaders: [
-      { 
-        test: /\.jsx?$/, 
-        loader: 'babel-loader', 
-        exclude: /node_modules/,
-      },
-      { 
-        test: /\.css$/, 
-        loader: 'style-loader!css-loader'
-      }
-    ],
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader'
-          },
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'assets')
+    },
+    module: {
+        loaders: [
+            { 
+                test: /\.jsx?$/, 
+                loader: 'babel-loader', 
+                exclude: /node_modules/,
+            },
+            { 
+                test: /\.css$/, 
+                loader: 'style-loader!css-loader'
             }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader'
-      }
-    ],
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    HtmlWebpackPluginConfig,
-    new UglifyJSPlugin()
-  ]
+        ],
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    },
+                    {
+                        loader: 'eslint-loader',
+                        options: {
+                            fix: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader'
+            }
+        ],
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new UglifyJSPlugin(),
+        new webpack.ProvidePlugin({
+            '$': 'jquery/dist/jquery.js',
+            'jQuery': 'jquery/dist/jquery.js'
+        })
+    ]
 };
